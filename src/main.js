3e7844f933389675c2a5c76734ac2ad3fe23cffb
semicvetik <semikirina@ukr.net>
2025-06-06 20:05:10 +0200

    test

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './js/refs.js';
+import './js/handlers.js';
+import './js/api-service.js';
+import './js/render-function.js';
+import './js/modal.js';
+import './js/feedback.js';
+import './js/menu.js';
+import './js/artists.js';

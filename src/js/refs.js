4d77782d145c24f3100a9db1228155ab47cd649a
Ira <semikirina@ukr.net>
2025-06-09 09:18:07 +0200

    add some updates

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,12 @@
 //Обʼєкти з посиланнями на ДОМ елементи
 export const refs = {
+  // Header & Navigation
+  burger: document.querySelector('.js-burger'),
+  closeBtn: document.querySelector('.js-close'),
+  navList: document.querySelector('.js-nav-list'),
+  navLinks: document.querySelectorAll('.nav-button'),
+  header: document.querySelector('.header'),
+
   // Hero section
   heroSection: document.querySelector('.js-hero'),
   getStartedBtn: document.querySelector('.js-hero-button'),

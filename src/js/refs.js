2a039d70172128390dc05aa4b8d2de122871d80a
Ira <semikirina@ukr.net>
2025-06-09 17:43:47 +0200

    update refs

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,32 +1,37 @@
-
 //Обʼєкти з посиланнями на ДОМ елементи
 export const refs = {
-    // Hero section
-    heroSection: document.querySelector('.js-hero'),
-    getStartedBtn: document.querySelector('.js-hero-button'),
-  
-    // Artists list
-    artistCardsContainer: document.querySelector('.js-artist-list'),
-    artistCards: document.querySelectorAll('.js-artist-card'),
-  
-    // About Artist Modal
-    modal: document.querySelector('.js-modal'),
-    modalCloseBtn: document.querySelector('.js-modal-close'),
-    modalContent: document.querySelector('.js-modal-content'),
-    modalFilters: document.querySelector('.js-modal-filters'),
-  
-    // About section
-    aboutSection: document.querySelector('.js-about'),
-    aboutImage: document.querySelector('.js-about-image'),
-    aboutText: document.querySelector('.js-about-text'),
-  
-    // Feedback
-    testimonialsSection: document.querySelector('.js-testimonials'),
-    testimonialsSlider: document.querySelector('.js-testimonials-slider'),
-    testimonialPrevBtn: document.querySelector('.js-testimonials-btn-prev'),
-    testimonialNextBtn: document.querySelector('.js-testimonials-btn-next'),
-  
-    // Footer
-    footer: document.querySelector('.js-footer'),
-  };
+  // Header & Navigation
+  burger: document.querySelector('.js-burger'),
+  closeBtn: document.querySelector('.js-close'),
+  navList: document.querySelector('.js-nav-list'),
+  navLinks: document.querySelectorAll('.nav-button'),
+  header: document.querySelector('.header'),
 
+  // Hero section
+  heroSection: document.querySelector('.js-hero'),
+  getStartedBtn: document.querySelector('.js-hero-button'),
+
+  // Artists list
+  artistCardsContainer: document.querySelector('.js-artist-list'),
+  artistCards: document.querySelectorAll('.js-artist-card'),
+  loadMoreBtn: document.querySelector('.load-more-btn'),
+
+  // About Artist Modal
+  modal: document.querySelector('.js-modal'),
+  modalCloseBtn: document.querySelector('.js-modal-close'),
+  modalContent: document.querySelector('.js-modal-content'),
+  modalFilters: document.querySelector('.js-modal-filters'),
+
+  // About section
+  aboutSection: document.querySelector('.js-about'),
+  aboutImage: document.querySelector('.js-about-image'),
+  aboutText: document.querySelector('.js-about-text'),
+
+  // Feedback
+  testimonialsSection: document.querySelector('.js-testimonials'),
+  testimonialsSlider: document.querySelector('.js-testimonials-slider'),
+  testimonialPrevBtn: document.querySelector('.js-testimonials-btn-prev'),
+  testimonialNextBtn: document.querySelector('.js-testimonials-btn-next'),
+  // Footer
+  footer: document.querySelector('.js-footer'),
+};

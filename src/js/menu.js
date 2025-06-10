e6f192c9ac1995cb1649956b627114d960591761
Max Yurchenko <yurchenko.maksym@gmail.com>
2025-06-10 20:53:45 +0100

    header add ID

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,33 +1,37 @@
-const burger = document.querySelector('.js-burger');
-const closeBtn = document.querySelector('.js-close');
-const navList = document.querySelector('.js-nav-list');
-const navLinks = document.querySelectorAll('.nav-button');
-const header = document.querySelector('.header');
+import { refs } from './refs.js';
 
-// Відкриття бургер-меню
-burger.addEventListener('click', () => {
-  navList.classList.add('is-open');
+
+refs.burger.addEventListener('click', () => {
+  refs.navList.classList.add('is-open');
 });
 
-// Закриття бургер-меню по хрестику
-closeBtn.addEventListener('click', () => {
-  navList.classList.remove('is-open');
+
+refs.closeBtn.addEventListener('click', () => {
+  refs.navList.classList.remove('is-open');
 });
 
-// Плавний перехід та закриття при кліку на пункт меню
-navLinks.forEach(link => {
+refs.navLinks.forEach(link => {
   link.addEventListener('click', e => {
     e.preventDefault();
     const targetId = link.getAttribute('href').substring(1);
     const targetEl = document.getElementById(targetId);
+
     if (targetEl) {
-      const headerHeight = header.offsetHeight;
-      const offsetTop = targetEl.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+      const headerHeight = refs.header.offsetHeight;
+      const offsetTop =
+        targetEl.getBoundingClientRect().top +
+        window.pageYOffset -
+        headerHeight;
+
       window.scrollTo({
         top: offsetTop,
-        behavior: 'smooth'
+        behavior: 'smooth',
       });
-      navList.classList.remove('is-open');
+
+
+      if (refs.navList.classList.contains('is-open')) {
+        refs.navList.classList.remove('is-open');
+      }
     }
   });
 });

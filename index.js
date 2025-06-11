13253fe9d50ff8051569ec0c40ff1efcb9d8c07c
iryna-kleimonova <semikirina@ukr.net>
2025-06-11 16:44:33 +0000

    Deploying to gh-pages from @ iryna-kleimonova/js-project-team-7@6aaca97943dbda33c3cc8ade936d3f7f00941d94 🚀

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ import{a as l,S as w,N as k,P as E}from"./assets/vendor-Cj1XzFCq.js";(function()
         </li>`}).join("");a.artistCardsContainer.insertAdjacentHTML("beforeend",s)}const I=document.querySelector(".artists-info"),u=document.querySelector(".artists-albums");function C(t){const{strArtist:s="-",strArtistThumb:r,intFormedYear:o,intDiedYear:e,strGender:n="-",intMembers:i,strCountry:c="-",strBiographyEN:d="-",genresList:f=[]}=t,v=o?e?`${o} - ${e}`:`${o} - present`:"-",L=f.length?f.map(S=>`<p class="genre-item">${S}</p>`).join(""):"-",$=`
     <h2 class="artist-title">${s}</h2>
     <div class="modal-artist-info">
-      <div class="artist-img-container">
+    <div class="artist-img-container">
         <img src="${r||""}" alt="${s}" />
       </div>
       <ul class="artist-meta-list"> 
@@ -34,11 +34,11 @@ import{a as l,S as w,N as k,P as E}from"./assets/vendor-Cj1XzFCq.js";(function()
           <li class="artist-meta-list-item"><b>Members</b><br>
             <p>${i??"-"}</p>
           </li>
-          <li class="artist-meta-list-item"<b>Country</b><br>
+          <li class="artist-meta-list-item"><b>Country</b><br>
             <p>${c}</p>
           </li>
       </ul>
-      
+    <div class="artist-bottom">
       <div class="artist-biography">
         <h3 class="artist-biography-title">Biography</h3>
         <p class="artist-biography-desc">${d}</p>
@@ -47,8 +47,10 @@ import{a as l,S as w,N as k,P as E}from"./assets/vendor-Cj1XzFCq.js";(function()
         ${L}
       </div>
     </div>
+    </div>
   `;I.innerHTML=$}function T(t=[]){if(u.innerHTML="",!t.length){u.innerHTML="<h4>-</h4>";return}const s=`
     <h2 class="albums-title">Albums</h2>
+      <div class="albums-list">
   ${t.map(r=>{const o=r.tracks&&r.tracks.length?`
               <ul class="track-list">
                 <li class="track-header">
@@ -73,6 +75,7 @@ import{a as l,S as w,N as k,P as E}from"./assets/vendor-Cj1XzFCq.js";(function()
             <h3 class="album-title">${r.strAlbum||"-"}</h3>
             ${o}
           </div>`}).join("")}
+    </div>
   `;u.innerHTML=s}function P(t){const s=Math.floor(t/1e3),r=Math.floor(s/60),o=s%60;return`${r}:${o.toString().padStart(2,"0")}`}function N(){a.loadMoreBtn.classList.remove("visually-hidden")}function x(){a.loadMoreBtn.classList.add("visually-hidden")}function F(){a.loader.classList.remove("visually-hidden")}function H(){a.loader.classList.add("visually-hidden")}function O(t,s){const r=typeof t=="string"?document.querySelector(t):t;if(!r)return;const o=document.createElement("div");o.className="stars";const e=Math.min(100,Math.max(0,s/5*100));o.style.setProperty("--rating-percent",`${e}%`),r.appendChild(o)}function D({artist:t,albums:s}){const r={...t,albumsList:s||[],genresList:t.genres||[]};C(r),T(r.albumsList),a.modal.classList.remove("hidden"),document.body.classList.add("modal-open")}document.addEventListener("click",t=>{t.target.closest(".js-modal-close")&&(a.modal.classList.add("hidden"),document.body.classList.remove("modal-open"),p())});document.addEventListener("keydown",t=>{t.key==="Escape"&&(a.modal.classList.add("hidden"),document.body.classList.remove("modal-open"),p())});a.modal.addEventListener("click",t=>{t.target===a.modal&&(a.modal.classList.add("hidden"),document.body.classList.remove("modal-open"),p())});function p(){document.removeEventListener("click",this),document.removeEventListener("keydown",this),a.modal.removeEventListener("click",this)}const R=new w(".swiper",{modules:[k,E],slidesPerView:1,spaceBetween:0,cssMode:!0,centeredSlides:!0,pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!1},navigation:{nextEl:".feedback-next",prevEl:".feedback-prev"},breakpoints:{320:{slidesPerView:1},768:{slidesPerView:1},1440:{slidesPerView:1}}});l.defaults.baseURL="https://sound-wave.b.goit.study/api/";async function U(){try{const{data:t}=await l.get("/feedbacks?limit=3&page=1"),s=t.data,r=document.getElementById("feedbacks-container");r.innerHTML="",s.forEach((o,e)=>{const n=document.createElement("div");n.classList.add("swiper-slide");const i=`raty-${e}`;n.innerHTML=`
         <div class="feedback-card">
           <div class="rating-star" id="${i}"></div>
